498848ac63f08d22270abe136ed034461a6a52e7
novice1011 <mqomaruzzaman@gmail.com>
2019-10-14 22:36:02 +0800

    checking messages from subscription using pthread

--- main.cpp
+++ main.cpp
@@ -1,34 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "mosquitto.h"
+#include <mosquitto.h>
+#include <pthread.h>
+
+void *checking_subs(void *arg){
+    struct mosquitto_message *msg;
+    struct mosquitto *mosq = mosquitto_new("laptop-ubuntu", 1, NULL);
+    while (1) {
+        int rc = mosquitto_subscribe_simple(
+                &msg, 1, true,
+                "#", 0,
+                "localhost", 1883,
+                NULL, 60, true,
+                NULL, NULL,
+                NULL, NULL);//im trying to understand
+        if(rc){
+            printf("Error: %s\n", mosquitto_strerror(rc));
+            mosquitto_lib_cleanup();
+        }
+        printf("%s %s\n", msg->topic, (char *)msg->payload);
+
+        mosquitto_message_free(&msg);
+    }
+
+}
 
 int main(int argc, char *argv[])
 {
-    int rc;
-    struct mosquitto_message *msg;
 
     mosquitto_lib_init();
 
-    rc = mosquitto_subscribe_simple(
-            &msg, 1, true,
-            "#", 0,
-            "localhost", 1883,
-            NULL, 60, true,
-            NULL, NULL,
-            NULL, NULL);//im trying to understand
+    pthread_t thread_checking_subs;
+    int err =pthread_create(&thread_checking_subs,NULL,checking_subs,NULL);
 
-    if(rc){
-        printf("Error: %s\n", mosquitto_strerror(rc));
-        mosquitto_lib_cleanup();
-        return rc;
-    }
-
-    printf("%s %s\n", msg->topic, (char *)msg->payload);
-    mosquitto_message_free(&msg);
+    printf("press any key to stop\n");
+    getchar();
 
     mosquitto_lib_cleanup();
-
     return 0;
 }
-
-//this is thread
