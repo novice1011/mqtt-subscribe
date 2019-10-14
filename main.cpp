8b80e5690871b4ce635bf8e42bcd0c1cd4c2a2de
novice1011 <mqomaruzzaman@gmail.com>
2019-10-14 16:13:51 +0800

    lets try commit a branch

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
             "localhost", 1883,
             NULL, 60, true,
             NULL, NULL,
-            NULL, NULL);//kkk
+            NULL, NULL);//im trying to understand
 
     if(rc){
         printf("Error: %s\n", mosquitto_strerror(rc));
@@ -30,3 +30,5 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
+//this is thread
